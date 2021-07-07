2a1a2606fd823c137e2a8d861fe481c0835cbd2c
hackerbb <85118856+hackerbb@users.noreply.github.com>
2021-07-08 01:39:55 +0530

    Add files via upload

---
+++ Day16/1/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import  "./style.css";
+// import  "./style1.css";
+
+import App1 from "./app1"
+// import App from "./app";
+ReactDOM.render(
+   <App1></App1>,
+    // <App></App>,
+ document.getElementsByTagName("BODY")[0]
+);
---
+++ Day16/2/src/app1.jsx
@@ -0,0 +1,10 @@
+function App1(){
+    return (
+        <div className = "card"> 
+        <div className = " name "> like</div>
+        <div class = "job">Dislike</div>
+        <div className ="h">Move</div>
+        </div>
+    );
+};
+export default App1
