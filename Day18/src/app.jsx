400bd6eb7cca3fd3ea3ed8b664b2001e8d3d4016
hackerbb <85118856+hackerbb@users.noreply.github.com>
2021-07-09 02:29:20 +0530

    Add files via upload

---
+++ Day18/src/app.jsx
@@ -0,0 +1,72 @@
+function App(){
+    return (
+        <div class="comp">
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+             <div class ="Box"></div>        
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>        
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+             <div class ="Box"></div>        
+            <div class ="Box"></div>
+            <div class ="Box"></div>
+            <div class ="Box"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>        
+            <div class ="Box1"></div>
+            <div class ="Box1"></div>       </div>
+    )
+}
+export default App
